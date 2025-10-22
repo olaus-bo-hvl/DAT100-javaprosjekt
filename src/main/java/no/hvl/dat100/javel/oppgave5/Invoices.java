83af7ebd5cd68b3e76bf7f14217a28443bef3f62
AreTho47 <are.thoresen1404@gmail.com>
2025-10-22 23:55:01 +0200

    Update Invoices.java

--- src/main/java/no/hvl/dat100/javel/oppgave5/Invoices.java
+++ src/main/java/no/hvl/dat100/javel/oppgave5/Invoices.java
@@ -3,8 +3,12 @@ package no.hvl.dat100.javel.oppgave5;
 public class Invoices {
 
     public static void processInvoices(Invoice[] invoices) {
-
-        // TODO
-
+        for (Invoice inv : invoices) {
+            System.out.println("========================");
+            inv.computeAmount();
+            inv.printInvoice();
+            System.out.println("========================");
+            System.out.println();
+        }
     }
 }
